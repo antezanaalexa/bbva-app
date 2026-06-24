2c470d358ca72e36b7dc0f2224049dfc5aa20ae7
antezanaalexa <antezanalexa37@gmail.com>
2026-06-23 19:25:10 -0500

    feat: versión inicial del proyecto BBVA Perú Simulado

--- web_front_homebanking_andino_react-main/src/components/ui/Loader.jsx
+++
@@ -1,9 +0,0 @@
-// Indicador de carga centrado.
-export default function Loader({ text = 'Cargando…' }) {
-  return (
-    <div className="hb-loader">
-      <div className="hb-spinner" />
-      <span>{text}</span>
-    </div>
-  )
-}
